cdfd5a4971e812a5a38d63566067b3c619c6b6ec
Daniel Nakolah <nakolahdaniel1@gmail.com>
2021-12-28 10:56:15 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using School;
+
+internal sealed class Playground
+{
+    static void Main(string[] args)
+    {
+        var daniel = new Student("Daniel", "Nakolah", "Oladidi");
+        var david = new Student("David", "Mkuu");
+        var faith = new Student("Faith", "Daniels");
+        var anna = new Student("Anna", "Nadupa", "Nakolahs");
+
+        var biology = new Subject("Biology");
+        var physics = new Subject("Physics");
+
+        biology.AddStudents(daniel, daniel, faith, anna);
+        physics.AddStudents(daniel, david, daniel, daniel, anna, faith);
+
+        var danielSimilarNames = daniel.FindStudentsWithSimilarNamesInClass();
+
+        foreach (var student in danielSimilarNames)
+            Console.WriteLine(student);
+    }
+}
